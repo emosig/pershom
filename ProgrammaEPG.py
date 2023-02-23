616a0c0de5d2a63a8d072f1eec04a7f04a502d3a
emosig <eloymosiggarcia@gmail.com>
2023-02-23 17:15:57 +0100

    Comitto tutto adesso che le cose funzionano ancora perché ho paura di romperlo tutto. Ho implementato due tipi di fasci di rette che intersecano l'EPG. Guardare test_rette.ipynb e test_clustering.ipynb

--- ProgrammaEPG.py
+++ ProgrammaEPG.py
@@ -5,15 +5,52 @@ from scipy import optimize
 import numdifftools as nd
 from ourPolynomial import *
 
-#Metodo ausiliare per chiarezza nella funzione Pareto. p = x[i].
+#Metodo ausiliare per chiarezza nella funzione Pareto_crit. p = x[i].
 def append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc):
     ppc.append(p)
     f1ppc.append(f_1.eval(p[0],p[1]))
     f2ppc.append(f_2.eval(p[0],p[1]))
     return True     #Questo true è per il booleano 'aggiunto'
 
+#Controlla se un punto è pareto-critico con tolleranza = tol
+def Pareto_crit(p,a,b,f_1,f_2,ppc,f1ppc,f2ppc,tol):
+    aggiunto = False    #Per evitare ripetere dei punti
+
+    #se ho un punto critico allora è anche pareto critico (infatti verifico con tolleranza p3)
+    if (abs(a[0])<=tol and abs(a[1])<=tol):
+        aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
+    if(abs(b[0])<=tol and abs(b[1])<=tol) and not aggiunto:
+        aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+    
+    #troviamo ora i punti dell'insieme di Jacobi che non sono critici e sono Pareto Critici
+    
+    if abs(a[0])<=tol and abs(a[1])>tol and not aggiunto:
+        if b[1]/a[1]<=tol:                   #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+            
+    if abs(a[1])<=tol and abs(a[0])>tol and not aggiunto:
+        if b[0]/a[0]<=tol:                   #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
+    if abs(b[0])<=tol and abs(b[1])>tol and not aggiunto:
+        if a[1]/b[1]<=tol:                   #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
+    if abs(b[1])<=tol and abs(b[0])>tol and not aggiunto:
+        if a[0]/b[0]<=tol:                   #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
+    if abs(b[0])>tol and abs(b[1])>tol and not aggiunto:
+        if a[0]/b[0]<=tol or a[1]/b[1]<=tol:  #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
+    if abs(a[0])>tol and abs(a[1])>tol and not aggiunto:
+        if b[0]/a[0]<=tol or b[1]/a[1]<=tol:  #sarebbe lambda<=0
+            aggiunto = append_aux(p,f_1,f_2,ppc,f1ppc,f2ppc)
+
 #definisco la funzione che calcola i punti critici e i punti pareto critici, gli do in input già i gradienti e dentro EPG decido come calcolarli
-def Pareto(f_1,f_2,p1,p2,p3,metodo):
+def Pareto(f_1,f_2,p1,p2,p3):
     #costruisco una griglia di p1*p1 punti sul toro [0,2pi]x[0,2pi]
     eps=2*np.pi/p1
     x=[]
@@ -33,17 +70,12 @@ def Pareto(f_1,f_2,p1,p2,p3,metodo):
     f1ppc=[] #I punti che verrano plottati (immagini dei ppc)
     f2ppc=[]
 
-    if metodo==True:
-        grf_1=f_1.gradient()
-        grf_2=f_2.gradient()
+    grf_1=f_1.gradient()
+    grf_2=f_2.gradient()
 
     for i in range (0,l,1):
-        if metodo==False:
-            a=nd.Gradient(f_1)(x[i])
-            b=nd.Gradient(f_2)(x[i])
-        if metodo==True:
-            a=[grf_1[0].eval(x[i][0],x[i][1]),grf_1[1].eval(x[i][0],x[i][1])]
-            b=[grf_2[0].eval(x[i][0],x[i][1]),grf_2[1].eval(x[i][0],x[i][1])]
+        a=[grf_1[0].eval(x[i][0],x[i][1]),grf_1[1].eval(x[i][0],x[i][1])]
+        b=[grf_2[0].eval(x[i][0],x[i][1]),grf_2[1].eval(x[i][0],x[i][1])]
         
         if abs(a[0])<=p2 and abs(a[1])<=p2:       #punto critico per f_1
             cr1.append(x[i])
@@ -54,41 +86,7 @@ def Pareto(f_1,f_2,p1,p2,p3,metodo):
         
         #la seguente condizione mi dice che ho un punto dell'insieme di Jacobi
         if abs(det[i])<=p3:
-
-            aggiunto = False    #Per evitare ripetere dei punti
-
-            #se ho un punto critico allora è anche pareto critico (infatti verifico con tolleranza p3)
-            if (abs(a[0])<=p3 and abs(a[1])<=p3):
-                aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-
-            if(abs(b[0])<=p3 and abs(b[1])<=p3) and not aggiunto:
-                aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-            
-            #troviamo ora i punti dell'insieme di Jacobi che non sono critici e sono Pareto Critici
-            
-            if abs(a[0])<=p3 and abs(a[1])>p3 and not aggiunto:
-                if b[1]/a[1]<=p3:                   #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-                    
-            if abs(a[1])<=p3 and abs(a[0])>p3 and not aggiunto:
-                if b[0]/a[0]<=p3:                   #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-
-            if abs(b[0])<=p3 and abs(b[1])>p3 and not aggiunto:
-                if a[1]/b[1]<=p3:                   #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-
-            if abs(b[1])<=p3 and abs(b[0])>p3 and not aggiunto:
-                if a[0]/b[0]<=p3:                   #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-
-            if abs(b[0])>p3 and abs(b[1])>p3 and not aggiunto:
-                if a[0]/b[0]<=p3 or a[1]/b[1]<=p3:  #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
-
-            if abs(a[0])>p3 and abs(a[1])>p3 and not aggiunto:
-                if b[0]/a[0]<=p3 or b[1]/a[1]<=p3:  #sarebbe lambda<=0
-                    aggiunto = append_aux(x[i],f_1,f_2,ppc,f1ppc,f2ppc)
+            Pareto_crit(x[i],a,b,f_1,f_2,ppc,f1ppc,f2ppc,p3)
 
     ppc=np.array(ppc)
     cr1=np.array(cr1)
@@ -96,36 +94,21 @@ def Pareto(f_1,f_2,p1,p2,p3,metodo):
     f1ppc=np.array(f1ppc)
     f2ppc=np.array(f2ppc)
 
-
     return ppc,cr1,cr2,x,f1ppc,f2ppc
 
-def EPG(f_1,f_2,p1,p2,p3,metodo):  #gli argomenti sono ourPolynomial
-    ppc,cr1,cr2,x,f1ppc,f2ppc=Pareto(f_1,f_2,p1,p2,p3,metodo)
-    m='nd.gradient'
-    if metodo==True:
-        m='gradient'
-        fig,axes=plt.subplots(1,4)
-        fig.set_size_inches(28,4)
-        axes[0].scatter([p[0] for p in cr1],[p[1] for p in cr1],s=8)
-        axes[0].set_title('Pt crit di f_1, '+m+' con tol='+str(p2))
-        axes[1].scatter([p[0] for p in cr2],[p[1] for p in cr2],s=8)
-        axes[1].set_title('Pt crit di f_2, '+m+' con tol='+str(p2))
-        axes[2].scatter([p[0] for p in ppc],[p[1] for p in ppc],s=0.2)
-        axes[2].set_title('Pt Pareto Critici, '+m+', tol='+str(p3))
-        axes[3].scatter([p for p in f1ppc],[q for q in f2ppc],s=0.2)
-        axes[3].set_title('Extended Pareto Grid')
-
-    else:
-        fig,axes=plt.subplots(1,4)
-        fig.set_size_inches(28,4)
-        axes[0].scatter([p[0] for p in cr1],[p[1] for p in cr1],s=8)
-        axes[0].set_title('Pt crit di f_1, '+m+' con tol='+str(p2))
-        axes[1].scatter([p[0] for p in cr2],[p[1] for p in cr2],s=8)
-        axes[1].set_title('Pt crit di f_2, '+m+' con tol='+str(p2))
-        axes[2].scatter([p[0] for p in ppc],[p[1] for p in ppc],s=0.2)
-        axes[2].set_title('Pt Pareto Critici, '+m+', tol='+str(p3))
-        axes[3].scatter([f_1(p) for p in ppc],[f_2(p) for p in ppc],s=0.2)
-        axes[3].set_title('Extended Pareto Grid')
+def EPG(f_1,f_2,p1,p2,p3):  #gli argomenti sono ourPolynomial
+    ppc,cr1,cr2,x,f1ppc,f2ppc=Pareto(f_1,f_2,p1,p2,p3)
+    m='gradient'
+    fig,axes=plt.subplots(1,4)
+    fig.set_size_inches(28,4)
+    axes[0].scatter([p[0] for p in cr1],[p[1] for p in cr1],s=8)
+    axes[0].set_title('Pt crit di f_1, '+m+' con tol='+str(p2))
+    axes[1].scatter([p[0] for p in cr2],[p[1] for p in cr2],s=8)
+    axes[1].set_title('Pt crit di f_2, '+m+' con tol='+str(p2))
+    axes[2].scatter([p[0] for p in ppc],[p[1] for p in ppc],s=0.2)
+    axes[2].set_title('Pt Pareto Critici, '+m+', tol='+str(p3))
+    axes[3].scatter([p for p in f1ppc],[q for q in f2ppc],s=0.2)
+    axes[3].set_title('Extended Pareto Grid')
 
     #TESTING (mi serve per capire la quantità di punti che stiamo plottando)
     return f1ppc,f2ppc
@@ -133,3 +116,52 @@ def EPG(f_1,f_2,p1,p2,p3,metodo):  #gli argomenti sono ourPolynomial
 #Mi serve capire quale è il più piccolo rettangolo che contiene i punti pareto critici
 def get_square(f1ppc,f2ppc):
     return min(f1ppc), max(f1ppc), min(f2ppc), max(f2ppc)
+
+#Metodo per ricalcolare i punti di un cluster associato a noise con tolleranza minore
+#Ad ogni iterazione riduco la tolleranza alla metà finché il cluster non diventa sufficientemente piccolo.
+#Cosa vuol dire abbastanza piccolo? Si potrebbe specificare con un ulteriore parametro
+#Per ora voglio ridurre i punti del cluster da 1/5 (vedere CONTROLLO alla fine del loop while)
+def recalculate_cluster(cluster,a,b,f_1,f_2,ppc,f1ppc,f2ppc,tol):
+    ridurre_ancora = True    #Finché non ho ridotto "abbastanza" i punti del cluster questo rimane True
+    i = 0       #Questo è un counter che arriva fino a 10 per evitare che il while vada avanti per sempre
+
+    while ridurre_ancora or i < 10:
+        tol = tol/2     
+
+        #Mi servono delle liste ausiliari di punti pareto-critici per controllare che sto
+        #effettivamente riducendo la quantità di punti nel cluster
+        temp_f1ppc = []
+        temp_f2ppc = []
+        temp_ppc = []
+        
+        #Queste cose io vorrei che fossero class variables
+        grf_1=f_1.gradient()
+        grf_2=f_2.gradient()
+
+        for p in cluster:
+            a=[grf_1[0].eval(p[0],p[1]),grf_1[1].eval(p[0],p[1])]       #Stessa cosa qua   
+            b=[grf_2[0].eval(p[0],p[1]),grf_2[1].eval(p[0],p[1])]
+            Pareto_crit(p,a,b,f_1,f_2,temp_ppc,temp_f1ppc,temp_f2ppc,tol)
+
+        #CONTROLLO: HO EFFETTIVAMENTE RIDOTTO LA QUANTITÀ DI PUNTI NEL CLUSTER?
+        if len(temp_ppc) < len(cluster)/5:
+            ridurre_ancora = False
+        else:
+            i = i+1
+    
+    if not ridurre_ancora:  #Cioè, se il loop si è fermato perché ho ridotto veramente il cluster
+        to_delete_f1 = list(zip(*cluster))[0]
+        to_delete_f2 = list(zip(*cluster))[1]
+        #to_delete_ppc ... bisognerebbe cambiare tante cose per cancellare anche di questa lista
+
+        #Adesso cancello di f1ppc, f2ppc i punti del cluster vecchio e metto quelli ricalcolati (che sono 4/5 di meno)
+        maskf1 = np.in1d(np.arange(f1ppc.size),to_delete_f1)
+        maskf2 = np.in1d(np.arange(f2ppc.size),to_delete_f2)
+        f1ppc = f1ppc[maskf1]
+        f1ppc = f1ppc[maskf2]
+
+        #Ricalcolare EPG e dipingere ... magari comparando on quella vecchia
+        #Dopo. Controlliamo che tutto va bene
+    return temp_f1ppc, temp_f2ppc
+
+
--- Rette.py
+++ Rette.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 #Modelizzo 2 tipi di fasci di rette: il fascio di tipo 1 è dato da n rette 
-# parallele (y-mx+a=0)_a che variano nel parametro a fissata una pendenza m,
+# parallele (y-mx+a=0)_a che variano nel parametro a fissata una pendenza m>0,
 # il fascio di tipo 2 è quello dato da n rette che passano per un punto fisso (a,b)
 # date da ((a,b)+v_k(x,y))_k che variano nel parametro k dove v_k = (cosk,senk) è
 # il vettore direttore della retta
@@ -13,54 +13,59 @@ import matplotlib.cm as cm
 #Ci vogliono due nuovi parametri di precisione: uno per quanto "grosse" vogliamo
 # considerare le rette e un'altro per la quantità di rette considerate. Per ora li tengo fissi:
 
-RETTE = 20  #quantità di rette
-EPS = 0.01  #quanto grosse sono le rette
+RETTE = 20  #quantità di rette 
+EPS = 0.2  #quanto grosse sono le rette
 
 #fisso anche per ora la pendenza m per il tipo 1 e il punto (a,b) per il tipo 2
 M = 1
 A,B = 0,1
 
+#Fisso una tolleranza per i "cluster" di punti vicini nelle rette. I cluster più grandi 
+# saranno interpretati come rumore
+CLUST_EPS = 0.1
+
 #Questo è soltanto per ora per vedere che le rette sono quelle che io voglio
 #In realtà noi queste intersezioni non le vorremmo vedere, sono per togliere l'errore
 PLOT = True
 
-#Intersezione con una retta di tipo 1
+#Intersezione con una retta di tipo 1 
 def intersect_line_type1(f1ppc,f2ppc,a,m=M):
     inter = []
     for p,q  in zip(f1ppc,f2ppc):
-        aggiunto = False
-        if abs(q-m*p + a) < EPS:
-            inter.append([p,q])
-            aggiunto = True
-        if abs(q-m*p - a) < EPS and not aggiunto:
-            inter.append([p,q])
-    return inter
+        if abs(q-m*p - a) < EPS: #In realtà sto facendo 2 rette pero i punti vanno nella stessa lista
+            if [p,q] not in inter:
+                inter.append([p,q])
+    return np.array(sorted(inter))
 
 #Intersezione con un fascio di rette tipo 1
 def intersect_sheaf_type1(f1ppc,f2ppc,m=M):
     intersection_list = []  #Lista di liste: intersezione di ogni retta considerata con l'EPG
 
-    #Mi calcolo il quadrato che voglio intersecare con le mie rette
+    #Mi calcolo il rango in cui voglio fare variare a
     minx,maxx,miny,maxy = get_square(f1ppc,f2ppc)
+    mina = maxy - m*minx
+    maxa = miny - m*maxx
 
     #Voglio variare a \in [miny,maxy]
-    for a in np.linspace(miny,maxy,RETTE):
-        intersection_list.append(intersect_line_type1(f1ppc,f2ppc,a,m))
-
+    for a in np.linspace(mina,maxa,RETTE):
+        line = intersect_line_type1(f1ppc,f2ppc,a,m)
+        intersection_list.append(line)
+    
     if PLOT:
         colors = cm.rainbow(np.linspace(0, 1, len(intersection_list)))
         for r,c in zip(intersection_list,colors):
-            plt.scatter([p[0] for p in r],[p[1] for p in r],s=4,color=c)
-
-    return intersection_list
+            plt.scatter([p[0] for p in r],[p[1] for p in r],s=8,color=c)
+    
+    return np.array(intersection_list, dtype=object)
 
 #Intersezione con una retta di tipo 2
 def intersect_line_type2(f1ppc,f2ppc,k,a=A,b=B):
     inter = []
     for p,q  in zip(f1ppc,f2ppc):
         if abs(np.sin(k)*(q-b) - np.cos(k)*(p-a)) < EPS:
-            inter.append([p,q])
-    return inter
+            if [p,q] not in inter:
+                inter.append([p,q])
+    return np.array(sorted(inter))
 
 #Intersezione con un fascio di rette tipo 2
 def intersect_sheaf_type2(f1ppc,f2ppc,a=A,b=B):
@@ -68,11 +73,51 @@ def intersect_sheaf_type2(f1ppc,f2ppc,a=A,b=B):
 
     #k lo faccio variare in [0,pi]
     for k in np.linspace(0,np.pi,RETTE):
-        intersection_list.append(intersect_line_type2(f1ppc,f2ppc,k,a,b))
+        line = intersect_line_type2(f1ppc,f2ppc,k,a,b)
+        intersection_list.append(line)
 
     if PLOT:
         colors = cm.rainbow(np.linspace(0, 1, len(intersection_list)))
         for r,c in zip(intersection_list,colors):
-            plt.scatter([p[0] for p in r],[p[1] for p in r],s=4,color=c)
+            plt.scatter([p[0] for p in r],[p[1] for p in r],s=8,color=c)
 
-    return intersection_list
+    return np.array(intersection_list, dtype=object)
+
+#Questo metodo trova i clusters 1d di punti in una retta
+def line_clusters(line):
+    clusters = []
+    curr_point = line[0]
+    curr_cluster = [curr_point]
+    for p in line[1:]:
+        if abs(p[0]) <= abs(curr_point[0]) + CLUST_EPS and abs(p[1]) <= abs(curr_point[1]) + CLUST_EPS:
+            curr_cluster.append(p)
+        else:
+            clusters.append(curr_cluster)
+            curr_cluster = [p]
+        curr_point = p
+    clusters.append(curr_cluster)
+    return np.array(clusters, dtype=object)
+
+#Metodo che dato un fascio di rette mi ritorna un fascio di liste di clusters di punti
+#(quei punti vicini in ogni retta)
+def sheaf_of_clusters(intersection_list):
+    cluster_list = []
+    for l in intersection_list:
+        if len(l)>1:    #Ignoro le rette che non intersecano sufficentemente l'EPG
+            cluster_list.append(line_clusters(l))
+
+    if PLOT:
+        #Plotto i clusters della prima retta significativa
+        cl_line = cluster_list[5]
+        colors = cm.rainbow(np.linspace(0, 1, len(cl_line)))
+        for cl,c in zip(cl_line,colors):
+            plt.scatter([p[0] for p in cl],[p[1] for p in cl],s=8,color=c)
+        plt.title("I diversi cluster in una delle rette del fascio.")
+
+    return np.array(cluster_list, dtype=object)
+
+
+
+
+
+
